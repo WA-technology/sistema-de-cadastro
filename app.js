302a240156473952ac1445b834cf3974513b4169
WA-technology <124681025+WA-technology@users.noreply.github.com>
2023-02-09 12:18:04 -0300

    Commit para testar o pull no git

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log('Rodando o sistema de gerenciamento de jogos');   
 console.log('Aplicação se conectando ao banco de dados');
 console.log('atualização teste');
+console.log('Commit para testar o pull no git');
